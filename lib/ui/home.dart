64d12d6e8eca4b4b97991be11d771cc5844d6d94
LVT DEL <votheluc01@gmail.com>
2023-12-05 02:46:00 +0700

    expanded img

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import '../Constants.dart';
 
@@ -27,7 +28,9 @@ class _HomeState extends State<Home> {
     keyboardVisibilityController.onChange.listen((bool visible) {
       _onKeyboardVisibleChange(visible);
     });
+
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -35,198 +38,200 @@ class _HomeState extends State<Home> {
 
     return Scaffold(
       backgroundColor: const Color(0xfff0f1f5),
-      body: SingleChildScrollView(
-        controller: _scrollController,
-        physics: const BouncingScrollPhysics(),
-        child: Container(
-          padding: const EdgeInsets.all(18),
-          width: size.width,
-          height: size.height + 1,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: 150,
-                child: RichText(
-                  text: const TextSpan(
-                      text: "Your",
-                      style: TextStyle(
-                          fontSize: 32,
-                          color: Constants.primaryColor,
-                          fontWeight: FontWeight.bold),
-                      children: <TextSpan>[
-                        TextSpan(
-                            text: " workout partner",
-                            style: TextStyle(color: Colors.black87))
-                      ]),
-                ),
+      body: Container(
+        padding: const EdgeInsets.all(18),
+        width: size.width,
+        height: size.height,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: 150,
+              child: RichText(
+                text: const TextSpan(
+                    text: "Your",
+                    style: TextStyle(
+                        fontSize: 32,
+                        color: Constants.primaryColor,
+                        fontWeight: FontWeight.bold),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: " workout partner",
+                          style: TextStyle(color: Colors.black87))
+                    ]),
               ),
-              SizedBox(
-                height: size.height * .3,
+            ),
+            Expanded(
+              child: SizedBox(
+                // height: size.height * .3,
                 child: Image.asset("assets/workout.png"),
               ),
-              Container(
-                height: size.height * .3,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                    // borderRadius: const BorderRadius.all(Radius.circular(20)),
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.grey,
-                        spreadRadius: 2,
-                        blurRadius: 20,
-                        offset: Offset(0, 0),
-                      )
-                    ]),
-                child: Stack(
-                  clipBehavior: Clip.none,
-                  children: [
-                    const Positioned(
-                        top: 10,
-                        left: 20,
-                        child: Text(
-                          "Login Here",
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.w600),
-                        )),
-                    Positioned(
-                        top: 50,
-                        left: 20,
-                        child: Column(
-                          children: [
-                            Row(
-                              children: [
-                                const Icon(
-                                  Icons.mail_outline,
-                                  color: Colors.grey,
-                                ),
-                                Container(
-                                  padding: const EdgeInsets.only(left: 10),
-                                  width: 300,
-                                  child: TextField(
-                                    // keyboardType: TextInputType.emailAddress,
-                                    textInputAction: TextInputAction.next,
-                                    // focusNode: _emailFocusNode,
-                                    onEditingComplete: () {
-                                      // Focus.of(context).requestFocus(_passwordFocusNode);
-                                      FocusScope.of(context).nextFocus();
-                                    },
-                                    cursorColor: Colors.grey,
-                                    style: const TextStyle(color: Colors.black54),
-                                    decoration: const InputDecoration(
-                                        border: InputBorder.none,
-                                        hintText: "example@gmail.com"),
-                                  ),
-                                )
-                              ],
-                            ),
-                            SizedBox(
-                              width: size.width * .8,
-                              child: const Divider(
-                                color: Colors.grey,
-                              ),
-                            )
-                          ],
-                        )),
-                    Positioned(
-                        top: 120,
-                        left: 20,
-                        child: Column(
-                          children: [
-                            Row(
-                              children: [
-                                const Icon(
-                                  Icons.lock_outline,
-                                  color: Colors.grey,
-                                ),
-                                Container(
-                                  padding: const EdgeInsets.only(left: 10),
-                                  width: 300,
-                                  child: TextField(
-                                    // keyboardType: TextInputType.visiblePassword,
-                                    textInputAction: TextInputAction.done,
-                                    onEditingComplete: () {
-                                      _onClickLogin(context);
-                                    },
-                                    obscureText: true,
-                                    cursorColor: Colors.grey,
-                                    style: const TextStyle(
-                                        color: Colors.black54,
-                                        fontSize: 20,
-                                        letterSpacing: 1.4),
-                                    decoration: const InputDecoration(
-                                      border: InputBorder.none,
-                                      hintText: "-----------",
-                                    ),
-                                  ),
-                                )
-                              ],
-                            ),
-                            SizedBox(
-                              width: size.width * .8,
-                              child: const Divider(
-                                color: Colors.grey,
-                              ),
-                            )
-                          ],
-                        )),
-                    Positioned(
-                      top: 180,
+            ),
+            // Expanded(
+            //   // height: size.height * .3,
+            //   child: Image.asset("assets/workout.png"),
+            // ),
+            Container(
+              height: size.height * .3,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  // borderRadius: const BorderRadius.all(Radius.circular(20)),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.grey,
+                      spreadRadius: 2,
+                      blurRadius: 20,
+                      offset: Offset(0, 0),
+                    )
+                  ]),
+              child: Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  const Positioned(
+                      top: 10,
                       left: 20,
-                      child: SizedBox(
-                        width: size.width * .8,
-                        child: const Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "Create Account",
-                              style: TextStyle(color: Colors.black54),
+                      child: Text(
+                        "Login Here",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w600),
+                      )),
+                  Positioned(
+                      top: 50,
+                      left: 20,
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              const Icon(
+                                Icons.mail_outline,
+                                color: Colors.grey,
+                              ),
+                              Container(
+                                padding: const EdgeInsets.only(left: 10),
+                                width: 300,
+                                child: TextField(
+                                  // keyboardType: TextInputType.emailAddress,
+                                  textInputAction: TextInputAction.next,
+                                  // focusNode: _emailFocusNode,
+                                  onEditingComplete: () {
+                                    // Focus.of(context).requestFocus(_passwordFocusNode);
+                                    FocusScope.of(context).nextFocus();
+                                  },
+                                  cursorColor: Colors.grey,
+                                  style: const TextStyle(color: Colors.black54),
+                                  decoration: const InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: "example@gmail.com"),
+                                ),
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            width: size.width * .8,
+                            child: const Divider(
+                              color: Colors.grey,
                             ),
-                            Text(
-                              "Forgot Password",
-                              style: TextStyle(color: Colors.black54),
-                            )
-                          ],
-                        ),
+                          )
+                        ],
+                      )),
+                  Positioned(
+                      top: 120,
+                      left: 20,
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              const Icon(
+                                Icons.lock_outline,
+                                color: Colors.grey,
+                              ),
+                              Container(
+                                padding: const EdgeInsets.only(left: 10),
+                                width: 300,
+                                child: TextField(
+                                  // keyboardType: TextInputType.visiblePassword,
+                                  textInputAction: TextInputAction.done,
+                                  onEditingComplete: () {
+                                    _onClickLogin(context);
+                                  },
+                                  obscureText: true,
+                                  cursorColor: Colors.grey,
+                                  style: const TextStyle(
+                                      color: Colors.black54,
+                                      fontSize: 20,
+                                      letterSpacing: 1.4),
+                                  decoration: const InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: "-----------",
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            width: size.width * .8,
+                            child: const Divider(
+                              color: Colors.grey,
+                            ),
+                          )
+                        ],
+                      )),
+                  Positioned(
+                    top: 180,
+                    left: 20,
+                    child: SizedBox(
+                      width: size.width * .8,
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Create Account",
+                            style: TextStyle(color: Colors.black54),
+                          ),
+                          Text(
+                            "Forgot Password",
+                            style: TextStyle(color: Colors.black54),
+                          )
+                        ],
                       ),
                     ),
-                    Positioned(
-                      bottom: -30,
-                      right: 30,
-                      child: GestureDetector(
-                        behavior: HitTestBehavior.translucent,
-                        onTap: () {
-                          _onClickLogin(context);
-                        },
-                        child: Container(
-                          padding: const EdgeInsets.all(22),
-                          height: 60,
-                          width: 60,
-                          decoration: const BoxDecoration(
-                            gradient: LinearGradient(
-                                colors: [
-                                  Color(0xff382743),
-                                  Color(0xffff4590)
-                                ],
-                                stops: [
-                                  0,
-                                  1
-                                ],
-                                begin: FractionalOffset.topLeft,
-                                end: FractionalOffset.bottomRight),
-                            shape: BoxShape.circle,
-                          ),
-                          child: Image.asset("assets/right-arrow.png"),
+                  ),
+                  Positioned(
+                    bottom: -30,
+                    right: 30,
+                    child: GestureDetector(
+                      behavior: HitTestBehavior.translucent,
+                      onTap: () {
+                        _onClickLogin(context);
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(22),
+                        height: 60,
+                        width: 60,
+                        decoration: const BoxDecoration(
+                          gradient: LinearGradient(
+                              colors: [
+                                Color(0xff382743),
+                                Color(0xffff4590)
+                              ],
+                              stops: [
+                                0,
+                                1
+                              ],
+                              begin: FractionalOffset.topLeft,
+                              end: FractionalOffset.bottomRight),
+                          shape: BoxShape.circle,
                         ),
+                        child: Image.asset("assets/right-arrow.png"),
                       ),
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
@@ -237,12 +242,12 @@ class _HomeState extends State<Home> {
   }
 
   _onKeyboardVisibleChange(visible) async {
-    if (visible == true) {
-      await Future.delayed(const Duration(milliseconds: 400));
-      _scrollController.animateTo(
-          350, duration: const Duration(milliseconds: 200),
-          curve: Curves.linear);
-    }
+    // if (visible == true) {
+    //   await Future.delayed(const Duration(milliseconds: 400));
+    //   _scrollController.animateTo(
+    //       350, duration: const Duration(milliseconds: 200),
+    //       curve: Curves.linear);
+    // }
   }
 
   Future<void> _showMyDialog(BuildContext context) async {
